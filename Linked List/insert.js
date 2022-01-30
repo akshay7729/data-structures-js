4bd8fa4d15f76f5fdf882eed4c0af9129405433d
Akshay Khobragade <akshay.khobragade7875@gmail.com>
2022-01-30 13:16:13 +0530

    Linkedlist - Get, Set and Insert

---
+++ Linked List/insert.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return null;
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  insert(index, data) {
+    if (index < 0 || index > this.length + 1) {
+      return undefined;
+    } else {
+      let current = this.head;
+      let newNode = new Node(data);
+      if (index === 0) {
+        this.head = newNode;
+        newNode.next = current;
+      } else if (index === this.length) {
+        this.tail = newNode;
+        this.tail.next = null;
+      } else {
+        let currentNode = this.get(index);
+        for (let i = 1; i <= index; i++) {
+          current = current.next;
+          if (i === index - 1) {
+            current.next = newNode;
+            newNode.next = currentNode;
+          }
+        }
+      }
+      this.length++;
+    }
+
+    return this;
+  }
+}
+
+const list = new LinkedList();
+list.push("0");
+list.push("1");
+list.push("2");
+list.push("3");
+list.push("4");
+
+console.log(list.insert(2, "hello"));
---
+++ Linked List/set.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return null;
+    }
+
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  // just setting the value not inserting
+  set(index, data) {
+    const newNode = new Node(data);
+
+    const foundNode = this.get(index);
+    if (foundNode) {
+      return (foundNode.data = data);
+    }
+    return false;
+  }
+}
+
+const list = new LinkedList();
+list.push("1");
+list.push("2");
+list.push("3");
+list.push("4");
+list.push("5");
+
+console.log(list.set(0, "set"));
